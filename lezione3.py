360d094b66c28ba72be65d70359c30e681480d92
lorenzoMaggi7 <lorenzomaggi4@gmail.com>
2024-04-28 11:59:27 +0200

    esercizio 4-12 fatto

--- lezione3.py
+++ lezione3.py
@@ -96,7 +96,7 @@ print(" ")
 x = [i ** 3 for i in range(1, 11)]
 
 print(x)
-
+print(" ")
 """
 4-10. Slices: Using one of the programs you wrote in this chapter, 
 add several lines to the end of the program that do the following:
@@ -121,7 +121,7 @@ middle_index = len(cubes) // 2
 print("Three items from the middle of the list are:", cubes[middle_index:middle_index + 3])
 
 print("The last three items in the list are:", cubes[-3:])
-
+print(" ")
 """
 4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1. 
 Make a copy of the list of pizzas, and call it friend_pizzas. Then, do the following:
@@ -131,3 +131,33 @@ Make a copy of the list of pizzas, and call it friend_pizzas. Then, do the follo
 and then use a for loop to print the first list. Print the message My friend’s favorite pizzas are:, 
 and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list.
 """
+friend_pizzas = pizzas[:]
+pizzas.append("Marinara")
+friend_pizzas.append("4 stagioni")
+
+for i in range(len(pizzas)):
+    print(f"My favorite pizzas are: {pizzas[i]}")
+
+for i in range(len(friend_pizzas)):
+    print(f"My friend's favorite pizzas are: {friend_pizzas[i]}")
+
+print(" ")
+
+"""
+4-12. More Loops: 
+All versions of foods.py in this section have avoided using for loops when printing, to save space.
+ Choose a version of foods.py, and write two for loops to print each list of foods.
+"""
+my_foods = ['pizza', 'pasta', 'sushi']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('gelato')
+
+print("My favorite foods are:")
+for food in my_foods:
+    print("- " + food)
+
+print("\nMy friend's favorite foods are:")
+for food in friend_foods:
+    print("- " + food)
