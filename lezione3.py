f7dae23eb21d9725973682192d15824037b8bcb8
lorenzoMaggi7 <lorenzomaggi4@gmail.com>
2024-04-29 13:58:33 +0200

    esercizio 5-6 fatto

--- lezione3.py
+++ lezione3.py
@@ -161,3 +161,191 @@ for food in my_foods:
 print("\nMy friend's favorite foods are:")
 for food in friend_foods:
     print("- " + food)
+
+"""
+4-15. Code Review: 
+Choose three of the programs you’ve written in this chapter and modify each one to comply with PEP 8.
+"""
+
+pizzas = ["Margherita", "Boscaiola", "4 Formaggi"]
+
+for pizza in pizzas:
+    print(f"I like {pizza}")
+
+print("I really love pizza. My personal favorite is", pizzas[0])
+print("But I also love pizza without sauce, like Boscaiola or Focaccia.")
+print("I think they're more versatile. For example, you can slice a focaccia and add some cheese and prosciutto")
+print(" ")
+
+
+animals = ["Dog", "Cat", "Horse"]
+
+for animal in animals:
+    print(animal)
+
+print(f"The {animals[0]} is very playful")
+print(f"The {animals[1]} is my favorite pet")
+print(f"The {animals[2]} is very elegant")
+print(" ")
+
+
+multiples_of_three = list(range(3, 31, 3))
+
+for number in multiples_of_three:
+    print(number)
+
+print(" ")
+
+"""
+5-1. Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your code
+should look something like this:
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+• Look closely at your results, and make sure you understand why each line
+evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another
+5 tests evaluate to False.
+"""
+
+# Define a variable
+car = 'subaru'
+
+print("Is car == 'subaru'? I predict True")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False")
+print(car == 'audi')
+
+print("\nIs car != 'toyota'? I predict True")
+print(car != 'toyota')
+
+print("\nIs car != 'subaru'? I predict False")
+print(car != 'subaru')
+
+print("\nIs car in ['subaru', 'toyota', 'honda']? I predict True")
+print(car in ['subaru', 'toyota', 'honda'])
+
+print("\nIs car not in ['subaru', 'toyota', 'honda']? I predict False")
+print(car not in ['subaru', 'toyota', 'honda'])
+
+print("\nIs the length of car equal to 6? I predict True")
+print(len(car) == 6)
+
+print("\nIs the length of car less than 5? I predict False")
+print(len(car) < 5)
+
+print("\nIs the length of car greater than 4? I predict True")
+print(len(car) > 4)
+
+print("\nIs the length of car not equal to 5? I predict False")
+print(len(car) != 5)
+print(" ")
+
+
+"""
+5-3. Alien Colors #1: Imagine an alien was just shot down in a game. 
+Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. 
+If it is, print a message that the player just earned 5 points.
+• Write one version of this program that passes the if test and another that fails. 
+(The version that fails will have no output.)
+"""
+alien_color = "green"
+
+if alien_color == "green":
+    print("Congratulations! You just earned 5 points")
+
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("Congratulations! You just earned 5 points")
+
+print(" ")
+
+"""
+5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+• Write one version of this program that runs the if block and another that runs the else block.
+"""
+alien_color= "green"
+
+if alien_color == "green":
+    print("Congratulations! You just earned 5 points for shooting the green alien")
+else:
+    print("Congratulations! You just earned 10 points")
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("Congratulations! You just earned 5 points for shooting the green alien")
+else:
+    print("\nCongratulations! You just earned 10 points for shooting the alien")
+print(" ")
+
+"""
+5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+
+• If the alien is green, print a message that the player earned 5 points.
+• If the alien is yellow, print a message that the player earned 10 points.
+• If the alien is red, print a message that the player earned 15 points.
+• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+"""
+
+alien_color= "green"
+
+if alien_color == "green":
+    print("Congratulations! You just earned 5 points for shooting the green alien")
+elif alien_color == "yellow":
+     print("Congratulations! You just earned 10 points for shooting the yellow alien")
+else:
+     print("Congratulations! You just earned 15 points for shooting the red alien")
+print(" ")
+alien_color= "yellow"
+
+if alien_color == "green":
+    print("Congratulations! You just earned 5 points for shooting the green alien")
+elif alien_color == "yellow":
+     print("Congratulations! You just earned 10 points for shooting the yellow alien")
+else:
+     print("Congratulations! You just earned 15 points for shooting the red alien")
+print(" ")
+alien_color= "red"
+
+if alien_color == "green":
+    print("Congratulations! You just earned 5 points for shooting the green alien")
+elif alien_color == "yellow":
+     print("Congratulations! You just earned 10 points for shooting the yellow alien")
+else:
+     print("Congratulations! You just earned 15 points for shooting the red alien")
+print(" ")
+"""
+5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. 
+Set a value for the variable age, and then:
+• If the person is less than 2 years old, print a message that the person is a baby.
+• If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+• If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+• If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+• If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+• If the person is age 65 or older, print a message that the person is an elder.
+"""
+age:int= 20
+
+if age < 2:
+    print("The person is a baby")
+elif age < 4:
+    print("The person is a toddler")
+elif age < 13:
+    print("The person is a kid")
+elif age < 20:
+    print("The person is a teenager")
+elif age < 65:
+    print("The person is an adult")
+else:
+    print("The person is an elder")
+print(" ")
