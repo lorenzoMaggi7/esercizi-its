b55c4c9c8f62f3a73e2d9ecdd11e954472c94b4f
lorenzoMaggi7 <lorenzomaggi4@gmail.com>
2024-04-30 17:02:59 +0200

    es 8-6 eseguito

--- lezione4.py
+++ lezione4.py
@@ -86,3 +86,63 @@ def display_message():
     print("In this chapter, we are learning about functions in Python.")
 
 display_message()
+print(" ")
+"""
+8-2. Favorite Book: Write a function called favorite_book() that accepts one parameter, title. 
+The function should print a message, such as "One of my favorite books is Alice in Wonderland". 
+Call the function, making sure to include a book title as an argument in the function call.
+"""
+def favorite_book(title):
+    print(f"My favorite book is {title}")
+
+favorite_book("Death note")
+
+"""
+8-3. T-Shirt: 
+Write a function called make_shirt() 
+that accepts a size and the text of a message that should be printed on the shirt. 
+The function should print a sentence summarizing the size of the shirt and the message printed on it. 
+Call the function once using positional arguments to make a shirt. 
+Call the function a second time using keyword arguments.
+"""
+def make_shirt(size, message):
+    print(f"The size of the shirt is {size}, and the message write on it is {message}")
+
+make_shirt("M", "Daje Roma")
+print(" ")
+"""
+8-4. Large Shirts: 
+Modify the make_shirt() function so that shirts are large by default with a message that reads I love Python. 
+Make a large shirt and a medium shirt with the default message, 
+and a shirt of any size with a different message.
+"""
+def make_shirt(size="large", message="I love Python"):
+    print(f"Shirt size: {size.title()}, Message: {message}")
+make_shirt()
+make_shirt(size="medium")
+make_shirt(size="small", message="Ciao")
+print(" ")
+"""
+8-5. Cities: Write a function called describe_city() that accepts the name of a city and its country. 
+The function should print a simple sentence, such as Reykjavik is in Iceland. 
+Give the parameter for the country a default value. 
+Call your function for three different cities, at least one of which is not in the default country.
+"""
+
+def describe_city(name, country="Country"):
+    print(f"{name} is in {country}")
+describe_city("Rome","Italy")
+describe_city("Acapulco","Messico")
+describe_city("Dubai")
+print(" ")
+"""
+8-6. City Names: Write a function called city_country() that takes in the name of a city and its country. 
+The function should return a string formatted like this: "Santiago, Chile". 
+Call your function with at least three city-country pairs, and print the values that are returned.
+"""
+def city_country(city, country):
+    print(f"{city},{country}")
+city_country("Rome","Italy")
+city_country("Milano","Italy")
+city_country("Las Palmas","Spain")
+print(" ")
